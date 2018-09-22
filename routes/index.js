658bb4f0ceb6afbad375a44120304764a57c2b73
ser1zw <azariahsawtikes@gmail.com>
2018-09-23 06:12:49 +0900

    add some DB access operations

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,22 @@ const ToDo = require('../models/todo')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   ToDo.findAll().then((todos) => {
-    console.log(`ToDo: ${todos.length}`)
-    res.render('index', { title: 'Express' });
+    res.render('index', { todos: todos })
+  })
+});
+
+router.post('/', function(req, res, next) {
+  const body = req.body
+  const todo = ToDo.build({
+    subject: req.body.subject,
+    description: req.body.description,
+    deadline: req.body.deadline,
+  })
+
+  todo.save().then(() => {
+    ToDo.findAll().then((todos) => {
+      res.render('index', { todos: todos })
+    })
   })
 });
 
