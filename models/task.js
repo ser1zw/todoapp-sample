e4c065aa0dd6524d77ceed0d36b69736d5dc9241
ser1zw <azariahsawtikes@gmail.com>
2018-09-23 08:31:15 +0900

    add moment.js

--- models/task.js
+++ models/task.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const moment = require('moment-timezone')
+
 const sequelize = new Sequelize(null, null, null, {
   dialect: 'sqlite',
   storage: 'db/todoapp-sample.db'
@@ -7,7 +9,12 @@ const sequelize = new Sequelize(null, null, null, {
 const Task = sequelize.define('Task', {
     subject: { type: Sequelize.STRING },
     description: { type: Sequelize.STRING },
-    deadline: { type: Sequelize.DATE },
+    deadline: {
+        type: Sequelize.DATE,
+        get() {
+            return moment(this.getDataValue('deadline'), 'YYYY-MM-DD HH:mm:ss.SSS ZZ').format('YYYY/MM/DD HH:mm')
+        }
+    },
 }, {
     tableName: 'tasks',
     timestamps: false
