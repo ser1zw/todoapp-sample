7ed7ee2dfdfa75c1a51f4e1daf4d2abb2b704c21
ser1zw <azariahsawtikes@gmail.com>
2018-09-23 07:38:04 +0900

    add daterangepicker
    http://www.daterangepicker.com/

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,3 +3,23 @@ const deleteTask = function(id) {
         .appendTo(document.body)
         .submit();
 }
+
+$(document).ready(function() {
+    const dateTimeFormat = 'YYYY/MM/DD HH:mm'
+    $('#deadline').daterangepicker({
+		singleDatePicker : true,
+		autoUpdateInput : false,
+		showDropdowns : true,
+		startDate : moment(),
+		locale : {
+			format : dateTimeFormat
+		},
+		timePicker : true,
+		timePicker24Hour : true,
+		timePickerSeconds : false,
+	}, function(start, end, label) {
+		$('#deadline').val(start.format(dateTimeFormat))
+	});
+
+    $('#deadline').attr('placeholder', dateTimeFormat)
+})
